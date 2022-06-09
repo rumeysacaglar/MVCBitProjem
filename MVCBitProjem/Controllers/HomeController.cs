3a6a3d3cef788dcda04e1a41cdf680ddde5b2a8f
rumeysacaglar <rum.caglar@gmail.com>
2022-06-09 23:55:28 +0300

    Add showcase page.

--- MVCBitProjem/Controllers/HomeController.cs
+++ MVCBitProjem/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace MVCBitProjem.Controllers
 
             return View();
         }
+        [AllowAnonymous]
+        public ActionResult HomePage()
+        {
+            return View();
+        }
     }
 }
--- MVCBitProjem/Controllers/LoginController.cs
+++ MVCBitProjem/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -12,6 +14,8 @@ namespace MVCBitProjem.Controllers
     [AllowAnonymous]
     public class LoginController : Controller
     {
+        WriterLoginManager wm = new WriterLoginManager(new EfWriterDal());
+
         [HttpGet]
         public ActionResult Index()
         {
@@ -41,8 +45,9 @@ namespace MVCBitProjem.Controllers
         [HttpPost]
         public ActionResult WriterLogin(Writer p)
         {
-            Context c = new Context();
-            var writeruserinfo = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+            //Context c = new Context();
+            //var writeruserinfo = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+            var writeruserinfo = wm.GetWriter(p.WriterMail, p.WriterPassword);
             if (writeruserinfo != null)
             {
                 FormsAuthentication.SetAuthCookie(writeruserinfo.WriterMail, false);
