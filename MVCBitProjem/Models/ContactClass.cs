a35a6e868d572c910ef4e7079ba5a9af01eb8cde
rumeysacaglar <rum.caglar@gmail.com>
2022-06-14 14:13:27 +0300

    Last Edits

---
+++ MVCBitProjem/Models/ContactClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCBitProjem.Models
+{
+    public class ContactClass
+    {
+        [Required(ErrorMessage ="İsim girmek zorunludur.")]
+        public string WriterName { get; set; }
+        public string WriterMail { get; set; }
+        [Required(ErrorMessage = "Konu girmek zorunludur.")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Mesaj girmek zorunludur.")]
+        public string Message { get; set; }
+        public DateTime ContactDate { get; set; }
+    }
+}
