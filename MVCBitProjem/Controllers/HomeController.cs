db711a66f858f6606e04313739dd8c1237daa244
rumeysacaglar <rum.caglar@gmail.com>
2022-06-23 13:20:36 +0300

    Contact and Error404 Page

--- MVCBitProjem/Controllers/HomeController.cs
+++ MVCBitProjem/Controllers/HomeController.cs
@@ -1,4 +1,9 @@
-﻿using DataAccessLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using MVCBitProjem.Models;
 using System;
 using System.Collections.Generic;
@@ -10,7 +15,10 @@ namespace MVCBitProjem.Controllers
 {
     public class HomeController : Controller
     {
+        ContactManager cm = new ContactManager(new EfContactDal());
         Context c = new Context();
+        ViewModel viewModel = new ViewModel();
+
         public ActionResult Index()
         {
             return View();
@@ -23,16 +31,54 @@ namespace MVCBitProjem.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+        [HttpPost]
+        public ActionResult Contact(ViewModel viewModel)
+        {
+            Contact c = new Contact();
+            c.Username = viewModel.UserName;
+            c.UserMail = viewModel.UserMail;
+            c.Subject = viewModel.Subject;
+            c.Message = viewModel.Message;
+            c.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            cm.ContactAdd(c);
+            return RedirectToAction("HomePage");
+        }
+
+        //public IActionResult WriterAdd(AddWriterImgDto dto) // model kullan +
+        //{
+
+        //    Writer w = new Writer();
+        //    if (dto.Image != null)
+        //    {
+        //        var extension = Path.GetExtension(dto.Image.FileName);
+        //        var newImageName = Guid.NewGuid() + extension;
+        //        var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newImageName);
+        //        var stream = new FileStream(location, FileMode.Create);
+        //        dto.Image.CopyTo(stream);
+        //        w.Image = newImageName;
+
+        //    }
+        //    w.Mail = dto.Mail;
+        //    w.Name = dto.Name;
+        //    w.Password = dto.Password;
+        //    w.About = dto.About;
+        //    w.Phone = dto.Phone;
+        //    w.City = dto.City;
+        //    w.Gender = dto.Gender;
+        //    w.Status = true;
+        //    wm.TAdd(w);
+        //    return RedirectToAction("Index", "Dashboard");
+        //}
+
         [AllowAnonymous]
         public ActionResult HomePage()
         {
-            ViewModel viewModel = new ViewModel();
+            
             viewModel.ContentCount = c.Contents.Count();
             viewModel.HeadingCount = c.Headings.Count();
             viewModel.MessageCount = c.Messages.Count();
--- MVCBitProjem/Models/ViewModel.cs
+++ MVCBitProjem/Models/ViewModel.cs
@@ -17,5 +17,12 @@ namespace MVCBitProjem.Models
         public int ContentCount { get; set; }
         public int WriterCount { get; set; }
         public int MessageCount { get; set; }
+
+
+        public string UserName { get; set; }
+        public string UserMail { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public DateTime ContactDate { get; set; }
     }
 }
